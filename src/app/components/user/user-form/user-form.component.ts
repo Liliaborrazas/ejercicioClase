a0d4d120ab038e14dfad3345b5905818883c59f6
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-03-08 18:58:01 +0100

    validation name

--- src/app/components/user/user-form/user-form.component.ts
+++ src/app/components/user/user-form/user-form.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { User } from './../../../shared/models/user.model';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css']
 })
-export class UserFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class UserFormComponent {
+  user: User = new User();
+  
 
 }
